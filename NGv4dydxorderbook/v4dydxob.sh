5f2e06bfdf5cbb9b7ed60de7aa9e253508fade7a
chiwalfrm <chiuljnk@hotmail.com>
2024-09-21 19:34:55 -0500

    Update v4dydxob.sh

--- NGv4dydxorderbook/v4dydxob.sh
+++ NGv4dydxorderbook/v4dydxob.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 WORKINGDIR=`dirname $0`
-if [ $# = 0 ]
-then
-        market=BTC-USD
-else
-        market=$1
-fi
 while true
 do
-        python3 -u $WORKINGDIR/v4dydxob.py $1
+        python3 -u $WORKINGDIR/v4dydxob.py $*
 done
